9b744b0915f543faf0d4d23a7d45774ae9ddc3fb
Brian Brewer <up672363@myport.ac.uk>
2014-03-04 09:44:27 +0000

    Updated to new drawing and node layout
    Now using Chic based node objects and drawing using Node.Dimension and
    NodeStyle

--- js/gnode.js
+++ js/gnode.js
@@ -1,54 +1,139 @@
 /* Graphic Node Design */
-newNode = function() {
-	return {
-		inputs: ["Input 1", "Input 2", "Input 3", "Input 4", "Input 5", "Input 6"],
-		outputs: ["Output 1", "Output 2"],
-		name: "Node Name",
-		x: 100.5,
-		y: 100.5
-	}
-}
+var newNode = function () {
+    "use strict";
+    return {
+        inputs: ["Input 1", "Input 2", "Input 3", "Input 4", "Input 5", "Input 6"],
+        outputs: ["Output 1", "Output 2"],
+        name: "Node Name",
+        x: 100.5,
+        y: 100.5
+    };
+};
 
-// Compute Sizes using Just Data
-var GNode = chic.Class.extend({
-	init: function () {
-		this.Input = [],
-		this.Output = []
-	},
-	render: function () {
-	}
+var Class = Class || chic.Class;
+
+// Object for holding all the style information
+var NodeStyle = {
+    FontSize: 12,
+    FontColor: "#FFFFFF",
+    FontFamily: "Arial",
+    LineWidth: 1,
+    LineColor: "",
+    FillColor: 0,
+    NodePadding: 5,
+    NodeMargin: 10,
+    BackgroundColor: "#555555",
+    TitleBackgroundColor: "#333333",
+    InputColorRequired: "#FF0000",
+    InputColorOptional: "#0000FF",
+    InputColorFilled: "#00FF00"
+};
+
+// Top Level Graphical Node Class
+var GNode = Class.extend({
+    init: function (x, y, title) {
+        this.Input = {};
+        this.Output = {};
+        this.Position = {
+            X: x,
+            Y: y
+        };
+        this.Title = title;
+        this.Dimension = {};
+    },
+    Rename: function (newName) {
+        this.Title = newName;
+    },
+    CalculateSize: function () {
+        var inputHeight = 0,
+            outputHeight = 0,
+            output,
+            input;
+
+        // Static Values
+        this.Dimension.NodeWidth = 128;
+
+        // Calculate Private Values
+        inputHeight += NodeStyle.NodePadding;
+        outputHeight += NodeStyle.NodePadding;
+        for (input in this.Input) {
+            inputHeight += NodeStyle.NodePadding + NodeStyle.FontSize;
+        }
+        for (output in this.Output) {
+            outputHeight += NodeStyle.NodePadding + NodeStyle.FontSize;
+        }
+
+        // Dynamic Values
+        this.Dimension.TitleHeight = NodeStyle.FontSize + NodeStyle.NodePadding * 2;
+        this.Dimension.InputHeight = inputHeight;
+        this.Dimension.OutputHeight = outputHeight;
+        this.Dimension.InputOutputHeight = inputHeight > outputHeight ? inputHeight : outputHeight;
+        this.Dimension.NodeHeight = this.Dimension.TitleHeight + this.Dimension.InputOutputHeight;
+    }
 });
 
+// Node for editing and drawing Single Shapes (3 -> 4 Sides)
 var ShapeNode = GNode.extend({
-	init: function () {
-		this.sup();
-		this.input[0] = {
-			val: null,
-			def: {
-				x: 10,
-				y: 10
-			},
-			name: "X1, Y1"
-		};
-	},
-	render: function () {
-	}
+    init: function (x, y) {
+        this.sup(x, y, "Shape Node");
+
+        // Outputs
+        this.Input.Point1 = new PointData(0, 0);
+        this.Input.Point2 = new PointData(0, 0);
+        this.Input.Point3 = new PointData(0, 0);
+        this.Input.Point4 = new PointData(0, 0);
+
+        // Values
+        this.PointCount = 3;
+
+        // Final
+        this.CalculateSize();
+    }
 });
 
-var VectorInput = chic.Class.extend({
-	init: function (x, y) {
-		this.X = x | 0;
-		this.Y = y | 0;
-	}
+// Node for tesselating shapes
+var ShapeTesselateNode = GNode.extend({
+    init: function (x, y) {
+        this.sup(x, y, "Shape Tesselation");
+
+        // Inputs
+        this.Input.Point1 = null;
+        this.Input.Point2 = null;
+        this.Input.Point3 = null;
+        this.Input.Point4 = null;
+
+        // Outputs
+        this.Output.ColorData = new ColorDataData();
+
+        // Values
+        this.PointCount = 3;
+    }
 });
 
-var ColorInput = chic.Class.extend({
-	init: function (data) {
-		this.Data = [];
-	}
-})
+// Node for single integer
+var NumberNode = GNode.extend({
+    init: function () {}
+});
 
-var IntegerInput = chic.Class.extend({
-	init: function () {
-	}
-})
+// Top Level Data Class for Input / Outputs
+var GData = Class.extend({
+    init: function () {
+    }
+});
+
+// Data for points (2D Vector)
+var PointData = GData.extend({
+    init: function (x, y) {
+        this.X = x;
+        this.Y = y;
+        this.Type = "Point";
+    }
+});
+
+// Data for Integer
+var NumberData = GData.extend({
+});
+
+// Stores RGB as a Nested Array
+var ColorDataData = GData.extend({
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var canvas,
-	context,
-	nodes = [];
+    context,
+    nodes = [];
 
 canvas = document.getElementById("app-canvas");
 canvas.width = $(".navbar-default").width();
@@ -8,54 +8,91 @@ canvas.height = $(".snap-content").height();
 context = canvas.getContext("2d");
 
 function draw() {
-	var nodeIndex,
-		currentNode,
-		fontSize = 10,
-		padding = 5,
-		margin = 10;
+    var nodeIndex,
+        currentNode,
+        fontSize = 10,
+        padding = 5,
+        margin = 10;
 
-	context.strokeStyle = "#333";
-	context.fillStyle = "#777";
-	canvas.width = canvas.width;
+    context.strokeStyle = "#333";
+    context.fillStyle = "#777";
+    canvas.width = canvas.width;
 
-	// Example Node Drawing Code
-	for (nodeIndex = 0; nodeIndex < nodes.length; nodeIndex++) {
-		currentNode = nodes[nodeIndex];
+    // // Example Node Drawing Code
+    // for (nodeIndex = 0; nodeIndex < nodes.length; nodeIndex++) {
+    //     currentNode = nodes[nodeIndex];
 
-		// Fill Node Box
-		context.fillStyle = "#bbb";
-		context.fillRect(currentNode.x, currentNode.y, 130 + margin * 2, 300);
+    //     // Fill Node Box
+    //     context.fillStyle = "#bbb";
+    //     context.fillRect(currentNode.x, currentNode.y, 130 + margin * 2, 300);
 
-		// Draw Node Box
-		context.rect(currentNode.x, currentNode.y, 130 + margin * 2, 300);
+    //     // Draw Node Box
+    //     context.rect(currentNode.x, currentNode.y, 130 + margin * 2, 300);
 
-		// Draw Title Box
-		context.fillStyle = "#ccc";
-		context.fillRect(currentNode.x, currentNode.y, 130 + margin * 2, padding * 2 + fontSize);
-		context.rect(currentNode.x, currentNode.y, 130 + margin * 2, padding * 2 + fontSize);
+    //     // Draw Title Box
+    //     context.fillStyle = "#ccc";
+    //     context.fillRect(currentNode.x, currentNode.y, 130 + margin * 2, padding * 2 + fontSize);
+    //     context.rect(currentNode.x, currentNode.y, 130 + margin * 2, padding * 2 + fontSize);
 
-		// Draw Title of Node
-		context.textAlign = "left";
-		context.textBaseline = "middle";
-		context.fillStyle = "#000";
-		context.fillText(currentNode.name, currentNode.x + 10, currentNode.y + 10);
+    //     // Draw Title of Node
+    //     context.textAlign = "left";
+    //     context.textBaseline = "middle";
+    //     context.fillStyle = "#000";
+    //     context.fillText(currentNode.name, currentNode.x + 10, currentNode.y + 10);
 
-		// Draw Inputs
-		for (i = 0; i < currentNode.inputs.length; i++) {
-			context.fillText(currentNode.inputs[i], currentNode.x + margin, currentNode.y + padding * 4 + fontSize * 1.5 + padding * (i * 2) + fontSize * i);
-		}
+    //     // Draw Inputs
+    //     for (i = 0; i < currentNode.inputs.length; i++) {
+    //         context.fillText(currentNode.inputs[i], currentNode.x + margin, currentNode.y + padding * 4 + fontSize * 1.5 + padding * (i * 2) + fontSize * i);
+    //     }
 
-		// Draw Preview Box
-		context.fillStyle = "#fff";
-		context.fillRect(currentNode.x + margin, currentNode.y + padding * 4 + fontSize * 1.5 + padding * (currentNode.inputs.length * 2) + fontSize * currentNode.inputs.length, 130, 130);
-		context.rect(currentNode.x + margin, currentNode.y + padding * 4 + fontSize * 1.5 + padding * (currentNode.inputs.length * 2) + fontSize * currentNode.inputs.length, 130, 130);
-	}
-	context.stroke();
+    //     // Draw Preview Box
+    //     context.fillStyle = "#fff";
+    //     context.fillRect(currentNode.x + margin, currentNode.y + padding * 4 + fontSize * 1.5 + padding * (currentNode.inputs.length * 2) + fontSize * currentNode.inputs.length, 130, 130);
+    //     context.rect(currentNode.x + margin, currentNode.y + padding * 4 + fontSize * 1.5 + padding * (currentNode.inputs.length * 2) + fontSize * currentNode.inputs.length, 130, 130);
+    // }
+    // context.stroke();
+
+    // New Drawing
+    for (nodeIndex = 0; nodeIndex < nodes.length; nodeIndex++) {
+        currentNode = nodes[nodeIndex];
+
+        // Fill Node
+        context.fillStyle = NodeStyle.BackgroundColor;
+        context.fillRect(currentNode.Position.X, currentNode.Position.Y, currentNode.Dimension.NodeWidth, currentNode.Dimension.NodeHeight);
+
+        // Fill Title
+        context.fillStyle = NodeStyle.TitleBackgroundColor;
+        context.fillRect(currentNode.Position.X, currentNode.Position.Y, currentNode.Dimension.NodeWidth, currentNode.Dimension.TitleHeight);
+
+        // Write Title
+        context.textAlign = "left";
+        context.textBaseline = "middle";
+        context.fillStyle = NodeStyle.FontColor;
+        context.fillText(currentNode.Title, currentNode.Position.X + NodeStyle.NodeMargin, currentNode.Position.Y + currentNode.Dimension.TitleHeight / 2);
+
+        // Write Input / Output
+        currentHeight = currentNode.Dimension.TitleHeight + NodeStyle.NodePadding;
+
+        for (nodeInput in currentNode.Input) {
+            currentHeight += NodeStyle.NodePadding;
+            context.fillText(currentNode.Input[nodeInput].Type, currentNode.Position.X + NodeStyle.NodeMargin, currentNode.Position.Y + currentHeight);
+            currentHeight += NodeStyle.FontSize;
+        }
+
+        currentInput = 0;
+        for (nodeOutput in currentNode.Outputs) {
+        }
+        //@TODO: Finish this
+
+        // Draw Input / Output Points
+        // Draw Circle!
+    }
 }
 
+// Test adding new Node
 $(".list-group-item").on("click", function () {
-	nodes.push(newNode());
-	draw();
+    nodes.push(new ShapeNode(100, 100));
+    draw();
 });
 
 // UI JavaScript
@@ -69,62 +106,62 @@ var snapper = new Snap({
 });
 
 $("#open-left").on("click", function () {
-	if (snapper.state().state == "left") {
-		snapper.close();
-	} else {
-		snapper.open("left");
-	}
+    if (snapper.state().state == "left") {
+        snapper.close();
+    } else {
+        snapper.open("left");
+    }
 });
 
 $("#open-right").on("click", function () {
-	if (snapper.state().state == "right") {
-		snapper.close();
-	} else {
-		snapper.open("right");
-	}
+    if (snapper.state().state == "right") {
+        snapper.close();
+    } else {
+        snapper.open("right");
+    }
 });
 
 function openDialog() {
-	$("#myModal").modal({
-		show: true,
-		keyboard: true
-	});
+    $("#myModal").modal({
+        show: true,
+        keyboard: true
+    });
 }
 
 canvas.addEventListener("mousedown", canvasDown);
 canvas.addEventListener("mouseup", canvasUp);
 
 var canvasOffsetX,
-	canvasOffsetY,
-	canvasID;
+    canvasOffsetY,
+    canvasID;
 
 function canvasDown (e) {
-	canvasX = e.clientX + (snapper.state().state == "left" ? -200 : 0) + (snapper.state().state == "right" ? 200 : 0);
-	canvasY = e.clientY;
+    canvasX = e.clientX + (snapper.state().state == "left" ? -200 : 0) + (snapper.state().state == "right" ? 200 : 0);
+    canvasY = e.clientY;
 
-	// Maybe search backwards to find newest first
-	for (i = 0; i < nodes.length; i++) {
-		if (canvasX > nodes[i].x && canvasX < nodes[i].x + 150 && canvasY > nodes[i].y && canvasY < nodes[i].y + 300) {
-			canvasOffsetX = canvasX - nodes[i].x;
-			canvasOffsetY = canvasY - nodes[i].y;
-			canvasID = i;
-			console.log("Found 'em!");
-			canvas.addEventListener("mousemove", canvasMove);
-			break;
-		}
-	}
+    // Maybe search backwards to find newest first
+    for (i = 0; i < nodes.length; i++) {
+        if (canvasX > nodes[i].Position.X && canvasX < nodes[i].Position.X + nodes[i].Dimension.NodeWidth && canvasY > nodes[i].Position.Y && canvasY < nodes[i].Position.Y + nodes[i].Dimension.NodeHeight) {
+            canvasOffsetX = canvasX - nodes[i].Position.X;
+            canvasOffsetY = canvasY - nodes[i].Position.Y;
+            canvasID = i;
+            console.log("Found 'em!");
+            canvas.addEventListener("mousemove", canvasMove);
+            break;
+        }
+    }
 }
 
 function canvasMove (e) {
-	canvasX = e.clientX + (snapper.state().state == "left" ? -200 : 0) + (snapper.state().state == "right" ? 200 : 0);
-	canvasY = e.clientY;
+    canvasX = e.clientX + (snapper.state().state == "left" ? -200 : 0) + (snapper.state().state == "right" ? 200 : 0);
+    canvasY = e.clientY;
 
-	nodes[canvasID].x = canvasX - canvasOffsetX;
-	nodes[canvasID].y = canvasY - canvasOffsetY;
+    nodes[canvasID].Position.X = canvasX - canvasOffsetX;
+    nodes[canvasID].Position.Y = canvasY - canvasOffsetY;
 
-	draw();
+    draw();
 }
 
 function canvasUp (e) {
-	canvas.removeEventListener("mousemove", canvasMove);
+    canvas.removeEventListener("mousemove", canvasMove);
 }
